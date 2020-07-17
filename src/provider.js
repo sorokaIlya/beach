d117bb8086fc7b0f6c52e5f7a84e304abb890949
rw_mitalcoi <st-agrail@yandex.ru>
2020-07-17 17:03:23 +0300

    Celsius metric system

--- src/provider.js
+++ src/provider.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
 
 export function fetchActualWeather(city) {
-   return axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city},UA&appid=dc7ada61b156a56a0a64e27fa1e84476`);
+   return axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city},UA&appid=dc7ada61b156a56a0a64e27fa1e84476&units=metric`);
 }
